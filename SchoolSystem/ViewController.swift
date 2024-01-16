e134db7fb52daa368e4542e12151d5fa769c043a
Milu Ann George <miluann@gmail.com>
2024-01-16 12:19:36 +0530

    Initial Commit

---
+++ SchoolSystem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SchoolSystem
+//
+//  Created by Milu Ann George on 16/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
